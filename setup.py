3ba46a79e247c3b07aa6164b1e02b86e1921eaa1
Thomas Grill <gr@grrrr.org>
2017-01-10 14:57:51 +0100

    Added setup module

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name = 'krippendorff_alpha',
+    version = '0.01',
+    author = "Thomas Grill",
+    author_email = "gr@grrrr.org",
+    maintainer = "Thomas Grill",
+    maintainer_email = "gr@grrrr.org",
+    description = "Python implementation of Krippendorff's alpha measure for interrated agreement",
+    license = "GNU General Public License v3 (GPLv3)",
+    url = "https://github.com/grrrr/krippendorff-alpha",
+    py_modules = ["krippendorff_alpha"],
+)
+
